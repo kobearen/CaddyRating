f29b31456d42559245e6d22aa308fc6e04bd2595
satoshi_kimura <>
2020-12-20 15:45:31 +0900

    [update]ゴルフ場への意見ページ手前まで

--- app/src/main/java/com/example/caddyrating/MyPageFragment.kt
+++ app/src/main/java/com/example/caddyrating/MyPageFragment.kt
@@ -25,10 +25,20 @@ class MyPageFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val backButton = view.findViewById<Button>(R.id.back_button)
-        backButton.setOnClickListener{
+        back_button.setOnClickListener{
             Log.d(TAG, "BackButton pressed!")
             fragmentManager?.popBackStack()
         }
+        opinionsGolfCourseButton.setOnClickListener{
+//            // Fragmentを生成するためのFragmentTransactionの準備をして
+//            val firstFragment = EvaluationInput()
+//            // FirstFragment.javaのonCreateView()がスタート
+//            val fragmentTransaction = supportFragmentManager.beginTransaction()
+//            // add()でフラグメントの追加、 add()の第一引数には、Fragmentの表示を行う場所のid、第二引数は表示したいFragmentを指定
+//            fragmentTransaction.add(R.id.fragment_container, firstFragment)
+//            // commit()で反映を行う
+//            fragmentTransaction.commit()
+        }
+        logoutButton.setOnClickListener{}
     }
 }
