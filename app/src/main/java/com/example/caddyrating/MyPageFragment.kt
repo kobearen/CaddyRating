7b56c0f3bce826c3420678c0a85ee002fc0e9942
satoshi_kimura <>
2020-12-20 16:26:24 +0900

    [update]画面遷移の実装

--- app/src/main/java/com/example/caddyrating/MyPageFragment.kt
+++ app/src/main/java/com/example/caddyrating/MyPageFragment.kt
@@ -5,9 +5,11 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 import kotlinx.android.synthetic.main.fragment_mypage.*
+
 
 class MyPageFragment : Fragment() {
 
@@ -29,16 +31,13 @@ class MyPageFragment : Fragment() {
             Log.d(TAG, "BackButton pressed!")
             fragmentManager?.popBackStack()
         }
-        opinionsGolfCourseButton.setOnClickListener{
-//            // Fragmentを生成するためのFragmentTransactionの準備をして
-//            val firstFragment = EvaluationInput()
-//            // FirstFragment.javaのonCreateView()がスタート
-//            val fragmentTransaction = supportFragmentManager.beginTransaction()
-//            // add()でフラグメントの追加、 add()の第一引数には、Fragmentの表示を行う場所のid、第二引数は表示したいFragmentを指定
-//            fragmentTransaction.add(R.id.fragment_container, firstFragment)
-//            // commit()で反映を行う
-//            fragmentTransaction.commit()
+        opinionsGolfCourseButton.setOnClickListener {
+            val sendOpinionFragment = SendOpinionFragment()
+            val fragmentTransaction = fragmentManager?.beginTransaction()
+            fragmentTransaction?.addToBackStack(null)
+            fragmentTransaction?.replace(R.id.fragment_container, sendOpinionFragment)
+            fragmentTransaction?.commit()
         }
-        logoutButton.setOnClickListener{}
+        logoutButton.setOnClickListener {}
     }
 }
