32596417443cfa2cfe38fa5b8fbe467bed14f6c9
satoshi_kimura <>
2020-12-20 15:34:05 +0900

    [update]testボタンの追加

--- app/src/main/java/com/example/caddyrating/MyPageFragment.kt
+++ app/src/main/java/com/example/caddyrating/MyPageFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_mypage.*
 
 class MyPageFragment : Fragment() {
 
