4e7c4bdbb473df7121fe2d50473ea3cd0c37d2ed
satoshi_kimura <>
2021-01-11 14:23:10 +0900

    [fix]画面遷移の修正

--- app/src/main/java/com/example/caddyrating/SendOpinionFragment.kt
+++ app/src/main/java/com/example/caddyrating/SendOpinionFragment.kt
@@ -1,23 +1,19 @@
 package com.example.caddyrating
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_mypage.*
 import kotlinx.android.synthetic.main.fragment_send_opinion.*
+import kotlinx.android.synthetic.main.fragment_send_opinion.back_button
 
 class SendOpinionFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        sendOpinionButton.setOnClickListener {
-            val sendCompletelyFragment = SendCompletelyFragment()
-            val fragmentTransaction = fragmentManager?.beginTransaction()
-            fragmentTransaction?.addToBackStack(null)
-            fragmentTransaction?.replace(R.id.fragment_container, sendCompletelyFragment)
-            fragmentTransaction?.commit()
-        }
     }
 
     override fun onCreateView(
@@ -25,5 +21,18 @@ class SendOpinionFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_send_opinion, container, false)
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        back_button.setOnClickListener {
+            fragmentManager?.popBackStack()
+        }
+        sendOpinionButton.setOnClickListener {
+            val sendCompletelyFragment = SendCompletelyFragment()
+            val fragmentTransaction = fragmentManager?.beginTransaction()
+            fragmentTransaction?.addToBackStack(null)
+            fragmentTransaction?.replace(R.id.fragment_container, sendCompletelyFragment)
+            fragmentTransaction?.commit()
+        }
     }
 }
