5fff5a90902fe4668fdac7eb0e7014599033391c
satoshi_kimura <>
2021-01-11 14:40:21 +0900

    [update]現在の年月日取得実装

--- app/src/main/java/com/example/caddyrating/SendCompletelyFragment.kt
+++ app/src/main/java/com/example/caddyrating/SendCompletelyFragment.kt
@@ -1,15 +1,21 @@
 package com.example.caddyrating
 
+import android.os.Build
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_send_completely.*
+import kotlinx.android.synthetic.main.fragment_send_opinion.*
+import kotlinx.android.synthetic.main.fragment_send_opinion.back_button
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.util.*
 
 class SendCompletelyFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,5 +27,21 @@ class SendCompletelyFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_send_completely, container, false)
+    }
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        back_button.setOnClickListener {
+            fragmentManager?.popBackStack()
+        }
+        button_toMypage.setOnClickListener {
+            val secondFragment = MyPageFragment()
+            val fragmentTransaction = fragmentManager?.beginTransaction()
+            fragmentTransaction?.addToBackStack(null)
+            fragmentTransaction?.replace(R.id.fragment_container, secondFragment)
+            fragmentTransaction?.commit()
+        }
+        val onlyDate: LocalDate = LocalDate.now()
+        sendday.text = onlyDate.toString()
     }
 }
