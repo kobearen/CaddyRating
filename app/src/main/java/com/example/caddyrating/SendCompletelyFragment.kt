714c8ef097014a975346cfe185417f6f27a721ac
satoshi_kimura <>
2021-01-11 14:19:49 +0900

    [add]画面遷移と画面の追加

---
+++ app/src/main/java/com/example/caddyrating/SendCompletelyFragment.kt
@@ -0,0 +1,25 @@
+package com.example.caddyrating
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class SendCompletelyFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_send_completely, container, false)
+    }
+}
--- app/src/main/java/com/example/caddyrating/SendOpinionFragment.kt
+++ app/src/main/java/com/example/caddyrating/SendOpinionFragment.kt
@@ -5,11 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_send_opinion.*
 
 class SendOpinionFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        sendOpinionButton.setOnClickListener {
+            val sendCompletelyFragment = SendCompletelyFragment()
+            val fragmentTransaction = fragmentManager?.beginTransaction()
+            fragmentTransaction?.addToBackStack(null)
+            fragmentTransaction?.replace(R.id.fragment_container, sendCompletelyFragment)
+            fragmentTransaction?.commit()
+        }
     }
 
     override fun onCreateView(
